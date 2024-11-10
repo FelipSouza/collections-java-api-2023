cbe6d443e005b4a6e2c0126059bfe4d98780f466
FelipSouza <felipe.souza.facco@gmail.com>
2024-11-10 15:09:44 -0500

    Implementação do problema SomaNumeros

--- src/list/SomaNumeros/SomaNumeros.java
+++ src/list/SomaNumeros/SomaNumeros.java
@@ -27,7 +27,33 @@ public class SomaNumeros {
         System.out.println("Resultado da soma dos números da lista é: " + somaNumeros);
     }
 
+    public int encontrarMaiorNumero(){
 
+        int numeroMaior = numerosInteiros.get(1).getNumero();
+
+        for(NumerosInteiros n: numerosInteiros){
+            if (n.getNumero() > numeroMaior){
+                numeroMaior = n.getNumero();
+            }
+        }
+        return  numeroMaior;
+    }
+
+    public int encontrarMenorNumero(){
+
+        int numeroMenor = numerosInteiros.get(1).getNumero();
+
+        for(NumerosInteiros n: numerosInteiros){
+            if (n.getNumero() < numeroMenor){
+                numeroMenor = n.getNumero();
+            }
+        }
+        return  numeroMenor;
+    }
+
+    public void exibirNumeros(){
+        System.out.println(numerosInteiros);
+    }
 
     public static void main(String[] args) {
         SomaNumeros somaNumeros = new SomaNumeros();
@@ -37,5 +63,8 @@ public class SomaNumeros {
         somaNumeros.adicionarNumero(9);
         somaNumeros.adicionarNumero(45);
         somaNumeros.calcularSoma();
+        System.out.println("O número maior da lista é: " + somaNumeros.encontrarMaiorNumero());
+        System.out.println("O número menor da lista é: " + somaNumeros.encontrarMenorNumero());
+        somaNumeros.exibirNumeros();
     }
 }
