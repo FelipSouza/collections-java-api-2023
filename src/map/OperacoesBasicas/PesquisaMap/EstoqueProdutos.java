beda35352adc991e21aba72605a9a0dfa5c49175
Felipe <felipe.souza.facco@gmail.com>
2024-11-12 19:56:37 -0500

    Imprementação Estoque produto vesão 2
    Signed-off-by: Felipe <felipe.souza.facco@gmail.com>

--- src/map/OperacoesBasicas/PesquisaMap/EstoqueProdutos.java
+++ src/map/OperacoesBasicas/PesquisaMap/EstoqueProdutos.java
@@ -28,5 +28,62 @@ public class EstoqueProdutos {
         return valorTotalEstoque;
     }
 
+    public Produto obterProdutoMaisCaro(){
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        if(!estoqueProdutosMap.isEmpty()) {
+            for (Produto p : estoqueProdutosMap.values()) {
+                if(p.getPreco()> maiorPreco){
+                    produtoMaisCaro = p;
+                }
+            }
+        }
+        return produtoMaisCaro;
+    }
 
+    public Produto obterProdutoMaisBarato(){
+        Produto produtoMaisBarato = null;
+        double menorPreco = 10.00;
+        if(!estoqueProdutosMap.isEmpty()){
+            for(Produto p : estoqueProdutosMap.values()){
+                if(p.getPreco()<menorPreco){
+                    produtoMaisBarato = p;
+                }
+            }
+        }
+        return produtoMaisBarato;
+    }
+
+    public Produto obterProdutoMaiorQuantidadeValorTotalNoEstoque(){
+        Produto produtorMaiorQuantidadeValorTotalNoEstoque = null;
+        double quantidadeXPreco = 0.0;
+        if(!estoqueProdutosMap.isEmpty()) {
+            for(Produto p : estoqueProdutosMap.values()) {
+                if( (p.getPreco()*p.getQuantidade()) > quantidadeXPreco){
+                    produtorMaiorQuantidadeValorTotalNoEstoque = p;
+                }
+            }
+        }
+        return produtorMaiorQuantidadeValorTotalNoEstoque;
+    }
+
+
+
+    public static void main(String[] args) {
+        EstoqueProdutos estoqueProdutos = new EstoqueProdutos();
+
+        estoqueProdutos.adicionarProduto(1, "Sabão", 30, 10.00);
+        estoqueProdutos.adicionarProduto(2, "Luva", 10, 12.00);
+        estoqueProdutos.adicionarProduto(3, "Frango", 10, 25.00);
+
+        estoqueProdutos.exibirProdutos();
+
+        System.out.println("Valor total do estoque: " + estoqueProdutos.calcularValorTotalEstoque());
+        System.out.println("Produto mais caro do estoque: " + estoqueProdutos.obterProdutoMaisCaro());
+        System.out.println("Produto mais barato do estoque: " + estoqueProdutos.obterProdutoMaisBarato());
+        System.out.println("Produto de quantidade no estoque (quant. * preço):  " + estoqueProdutos.obterProdutoMaiorQuantidadeValorTotalNoEstoque());
+
+
+
+    }
 }
